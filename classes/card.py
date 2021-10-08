c6a808e20ec2b57eb968e13500d8bf1b4ca647e2
Jakub Melzacki <melzacki.jakub@gmail.com>
2021-10-08 20:08:35 +0200

    added files and base of Card class

---
+++ classes/card.py
@@ -0,0 +1,33 @@
+from utils.values import COLOURS_REPR, RANKS_REPR, COLOURS_NAMES, RANKS_NAMES, ACTIVITY
+
+
+class Card:
+
+    def __init__(self, rank, colour):
+        self.rank = rank
+        self.colour = colour
+
+    def __repr__(self):
+        return f"[{RANKS_REPR[self.rank]} {COLOURS_REPR[self.colour]}]"
+
+    @property
+    def name(self):
+        return f"[{RANKS_NAMES[self.rank]} of {COLOURS_NAMES[self.colour]}s]"
+
+    def __eq__(self, other):
+        if self.rank == other.rank:
+            return True
+
+        if ACTIVITY[self.rank] == ACTIVITY[other.rank] and self.colour == other.colour:
+            return True
+
+        return False
+
+
+if __name__ == '__main__':
+    card1 = Card(5, 1)
+    card2 = Card(5, 2)
+    card3 = Card(6, 2)
+
+    print(card1 == card2)
+    print(card2 == card3)
